0e15619a8fa02dec7cc943b8ec4417dc1a7c62f7
347814 <54952912+347814@users.noreply.github.com>
2020-06-02 14:53:59 -0700

    Add files via upload

--- hashtable/hashTable.cpp
+++ hashtable/hashTable.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<fstream>
-#include <stdlib.h> 
+#include <stdlib.h>
+#include <cstdlib>
+#include <time.h>
 using namespace std;
 struct student{
   char* firstName;
@@ -11,20 +13,23 @@ struct student{
 
 
 };
-
-void chaining(student* k, student* current, int count);
+void chainPrint(student* current);
+void chaining(student* k, student* current, int count, student** students, int indecies);
 void addStudent(student* k, int &indecies, student** students);
 void randomStudent(student** students, int indecies, int &namecount, int &lastnamecount);
-
+void chaining(student* k, student* current, int count, student** students, int indecies);
+void rehash(int indecies, student* k, student** students);
+void print(student** students, int indecies);
 int main(){
   int namecount = 0; //tracks the number of randomly generated names. Knows how many spaces to pass over
   int lastnamecount = 0;
   int indecies = 100;
   student** students =  new student* [100];
-
-  for(int i = 0; i < 2 ; i++){
+  srand(time(NULL));
+  for(int i = 0; i < 200 ; i++){
   randomStudent(students, indecies, namecount, lastnamecount);
   }
+  print(students, indecies);
   
   
   
@@ -73,12 +78,12 @@ void addStudent(student* k, int &indecies, student** students){
   while( sum > indecies){
     sum = sum / 2 ;
   }
-  if(students[sum] != NULL){
+  if(students[sum] == NULL){
     students[sum] = k;
 
   }
   else{
-
+    chaining(k, students[sum], 0, students, indecies); //call the chaining function
 
   }
 
@@ -89,8 +94,32 @@ void randomStudent(student** students, int indecies, int &namecount, int &lastna
   student* random = new student;
   ifstream infile;
   infile.open("firstnames.txt.txt");
+  ifstream count;
+  
+  //this loop determines the number of names in the file for bounds for a random number
+  count.open("firstnames.txt.txt");
+  char c;
+  int names = 0;
+  
+  while(count >> noskipws >> c){
+    if(c == ' '){
+      names++;
+    }
+
+  }
+  // cout<<names<<endl;
+  count.close();
+
+
+
+
+
+  //  cout<<names<<endl;
+
+  namecount = rand() % names; //get a random number for the namecount
+  
   char a;
-  int spacecount = 0;
+  int spacecount = 0; //create a random number between 0 and the counted number of names
   int j = 0;
   char* readName = new char[100];
   while(infile >> noskipws >> a){
@@ -109,7 +138,8 @@ void randomStudent(student** students, int indecies, int &namecount, int &lastna
     else{     // a = ' '
       spacecount ++;
       if(spacecount > namecount){
-	namecount++;
+	//namecount++;
+	readName[j] = '\0';
 	break;
       }
       
@@ -122,17 +152,43 @@ void randomStudent(student** students, int indecies, int &namecount, int &lastna
 
   }
 
-  readName[sizeof(readName) / sizeof(readName[0])] = '\0';
-  cout<<sizeof(readName) / sizeof(readName[0])<<endl;
+  //readName[sizeof(readName) / sizeof(readName[0])] = '\0';
+  //cout<<sizeof(readName) / sizeof(readName[0])<<endl;
   
 
   random->firstName = readName;
   
-  cout<<random->firstName<<endl;
+  //cout<<random->firstName<<" ";
 
   //now do the same for last name
+
+  
+  char x;
+  ifstream lastcount;
+  int lastnames = 0;
+  lastcount.open("lastnames.txt.txt");
+  while(lastcount >> noskipws >> x){
+    if(x == ' '){
+      lastnames++;
+    }
+
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+  
   char b;
   spacecount = 0;
+  lastnamecount = rand() % lastnames;
   j = 0;
   ifstream lastNameFile;
   char* readLastName = new char[100];
@@ -153,7 +209,8 @@ void randomStudent(student** students, int indecies, int &namecount, int &lastna
     else{     // a = ' '
       spacecount ++;
       if(spacecount > lastnamecount){
-	lastnamecount++;
+	//lastnamecount++;
+	readLastName[j] = '\0';
 	break;
       }
       
@@ -172,45 +229,62 @@ void randomStudent(student** students, int indecies, int &namecount, int &lastna
 
 
   
-  cout<<random->lastName<<endl;;  
+  //cout<<random->lastName<<endl;;  
 
+
+
+
+
+  //now iterate the student id
+
+  int maxid = 0;
+  for(int i = 0; i < indecies; i ++){
+    if(students[i] != NULL){
+      if(students[i]->id > maxid){
+	maxid = students[i]->id;
+      }
+      
+
+  }
+
+
+
+
+
+
+  }
+
+  random->id = maxid;
+  //cout<<random->id<<endl;
+
+  //create a random GPA
+  
+  double u;
+  u = (double)rand() / (RAND_MAX ) * (5);
+  // cout<<u<<endl;
+  random->gpa = u;
+  //cout<<random->gpa<<endl;
+  random->next = NULL;
+  addStudent(random, indecies, students);
+
+  
   
 
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-void chaining(student* k, student* current, int count){
+void chaining(student* k, student* current, int count, student** students, int indecies){
   if(current->next == NULL){
     current->next = k;
   }
   else{
     count++;
     if(count < 3){
-      chaining(k, current->next, count);
+      chaining(k, current->next, count, students, indecies);
     }
     else{
-      //void rehash
+      rehash(indecies, k, students);
 
     }
 
@@ -229,9 +303,35 @@ void rehash(int indecies, student* k, student** students){
   }
   students = new student* [indecies*2];
   //use hashing algorithm to re hash everything
-  
+  for(int i = 0; i < elements; i++){
+   
+      
+  }
 
 
 
 
+}
+
+void print(student** students, int indecies){
+  for(int i = 0 ; i < indecies; i++){
+    if(students[i] != NULL){
+      if(students[i]->next == NULL){
+      cout<<students[i]->firstName<<" "<<students[i]->lastName<<", "<<students[i]->id<<", "<<students[i]->gpa<<endl;
+      }
+    else{
+      //call a recursive function to keep printing linked list
+      chainPrint(students[i]);
+    }
+    }
+
+  }
+
+}
+
+void chainPrint(student* current){
+ cout<<current->firstName<<" "<<current->lastName<<", "<<current->id<<", "<<current->gpa<<endl;
+ if(current->next != NULL){
+   chainPrint(current->next);
+ }
 }
